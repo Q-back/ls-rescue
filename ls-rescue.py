34b9170b2cd61a9a57a53276bb78489d22ec6e56
Q_back <trampaudio@gmail.com>
2017-12-05 13:34:37 +0100

    Added argparse option, need tests

--- ls-rescue.py
+++ ls-rescue.py
@@ -1,16 +1,33 @@
-import sys
+import sys, argparse
 
-ls1_filepath = sys.argv[1]
-ls2_filepath = sys.argv[2]
+def read_files(file1, file2):
+	ls1_filepath = file1
+	ls2_filepath = file2
+	
+	ls1_file = open(ls1_filepath, "r")
+	ls2_file = open(ls2_filepath, "r")
+	
+	ls1_file_content = ls1_file.read()
+	ls2_file_content = ls2_file.read()
+	
+	ls1 = ls1_file_content.split()
+	ls2 = ls2_file_content.split()
+	return (ls1, ls2)
 
-ls1_file = open(ls1_filepath, "r")
-ls2_file = open(ls2_filepath, "r")
 
-ls1_file_content = ls1_file.read()
-ls2_file_content = ls2_file.read()
-
-ls1 = ls1_file_content.split()
-ls2 = ls2_file_content.split()
+description = ""
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument("file1", default=None, help="first file with ls output", type=str)
+parser.add_argument("file2", default=None, help="second file with ls output", type=str)
+parser.add_argument("-f", "--file", action="store_true", help="Use files instead of standard input")
+args = parser.parse_args()
+if args.file and args.file1 and args.file2:
+	files_readed = read_files(args.file1, args,file2)
+	ls1 = files_readed[1]
+	ls2 = files_readed[2]
+if not args.file:
+	ls1 = input("Enter first ls output: ").split()
+	ls2 = input("Enter second ls output: ").split()
 
 modified = []
 # Not optimal. I'll have to change it
@@ -29,4 +46,3 @@ modified_items = ""
 for item in modified:
 	modified_items += item+" "
 print("MODIFIED ITEMS: "+modified_items)
-
