7a68efbd35775b0a1fbda43894335d894051291e
Q_back <trampaudio@gmail.com>
2017-12-06 10:25:32 +0100

    Probably fixed error when diff item was printent 2 times. Added support for --same

--- ls-rescue.py
+++ ls-rescue.py
@@ -25,12 +25,31 @@ def read_files(file1, file2):
 	ls2_file.close()
 	return (ls1, ls2)
 
+def format_output(analized):
+	analized_items = ""
+	if args.l:
+		i = 1
+		for item in analized:
+			analized_items += "\n"
+			if args.show_sources:
+				analized_items += item.source + " --> "			
+			analized_items += "("+str(i)+") " + item.content
+			i+=1
+	else:
+		for item in analized:
+			analized_items += item.content+" "
+	return analized_items
+
 try:
 	description = "Find differences between two ls outputs"
 	parser = argparse.ArgumentParser(description=description)
 	parser.add_argument("-l", action="store_true", help="Print result in list format")
-	parser.add_argument("-s", "--show-sources", action="store_true", dest="show_sources", help="Show source file next to every output item. Works only when -l used.")
-	parser.add_argument("-f", "--file", nargs=2, help="Use files instead of standard input. Example: ls-rescue -f file1.txt path/to/file2.txt")
+	parser.add_argument("-s", "--show-sources", action="store_true", dest="show_sources", 
+		help="Show source file next to every output item. Works only when -l used.")
+	parser.add_argument("--same", action="store_true", 
+		help="Print items that occur in both ls outputs instead of items that are different")
+	parser.add_argument("-f", "--file", nargs=2, 
+		help="Use files instead of standard input. Example: ls-rescue -f file1.txt path/to/file2.txt")
 	args = parser.parse_args()
 
 	ls1_filepath = ""
@@ -49,29 +68,34 @@ try:
 		ls2 = input("Enter second ls output: ").split()
 	
 	modified = []
+	modified_control = []
+	same = []
+	same_control = []
 	# Not optimal. I'll have to change it
 	for listed_item in ls1:
 		if not listed_item in ls2:
+			modified_control.append(listed_item) #Quick solution, not good.
 			modified.append(ListedItem(listed_item, ls1_filepath))
-	
-	for listed_item in ls2:
-		if not listed_item in ls1 and not listed_item in modified:
-			modified.append(ListedItem(listed_item, ls2_filepath))
-	
-	modified_items = ""
+		else:
+			if not listed_item in same_control: #Probably that check is not needed
+				same_control.append(listed_item) #Quick solution, not good.
+				same.append(ListedItem(listed_item, ls1_filepath))
 
-	if args.l:
-		i = 1
-		for item in modified:
-			modified_items += "\n"
-			if args.show_sources:
-				modified_items += item.source + " --> "			
-			modified_items += "("+str(i)+") " + item.content
-			i+=1
+	for listed_item in ls2:
+		if not listed_item in ls1 and not listed_item in modified_control:
+			modified_control.append(listed_item) #Quick solution, not good.
+			modified.append(ListedItem(listed_item, ls2_filepath))
+		else:
+			if not listed_item in same_control:
+				same_control.append(listed_item) #Quick solution, not good.
+				same.append(ListedItem(listed_item, ls2_filepath))
+
+	if args.same:
+		same_items = format_output(same)
+		print(str( len(same) ) + " SAME ITEMS: " + same_items)
 	else:
-		for item in modified:
-			modified_items += item.content+" "
-	print(str( len(modified) ) + " MODIFIED ITEMS: " + modified_items)
+		modified_items = format_output(modified)
+		print(str( len(modified) ) + " MODIFIED ITEMS: " + modified_items)
 
 except KeyboardInterrupt:
 	print("\nInterrupted by user. Exit.")
